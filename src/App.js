6b30b9fb936ef8f8e615723fb0c180e2dabca49e
Fahad Ahmed <fhdplsh032@gmail.com>
2020-07-08 21:26:52 +0600

    Product details page added.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 
 // import pages
 import HomePage from './Pages/HomePage';
+import ProductDetails from './Pages/ProductDetailsPage';
 
 export default function App() {
-  return (<HomePage />);
+  return (<ProductDetails />);
 }
