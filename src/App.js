dd406e7671bad7431748c2de04667e4321c65132
Fahad Ahmed <fhdplsh032@gmail.com>
2020-07-08 21:37:23 +0600

    Login Page added.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import React from "react";
 // import pages
 import HomePage from './Pages/HomePage';
 import ProductDetails from './Pages/ProductDetailsPage';
-import OrderPage from './Pages/AdminOrderPage';
+import OrderPage from './Pages/OrderPage';
+import LoginPage from './Pages/LoginPage';
 
 export default function App() {
-  return (<OrderPage />);
+  return (<LoginPage />);
 }
