24ef20751b9e38cfdaf84e44db0b9c437af1bbd8
Fahad Ahmed <fhdplsh032@gmail.com>
2020-07-08 21:36:09 +0600

    Order page added at user end.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from "react";
 // import pages
 import HomePage from './Pages/HomePage';
 import ProductDetails from './Pages/ProductDetailsPage';
+import OrderPage from './Pages/AdminOrderPage';
 
 export default function App() {
-  return (<ProductDetails />);
+  return (<OrderPage />);
 }
