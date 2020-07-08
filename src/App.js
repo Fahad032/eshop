bfb1574d4ff34f0473d7c978712d974756f68cba
Fahad Ahmed <fhdplsh032@gmail.com>
2020-07-08 21:48:51 +0600

    WIP: signup page.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import HomePage from './Pages/HomePage';
 import ProductDetails from './Pages/ProductDetailsPage';
 import OrderPage from './Pages/OrderPage';
 import LoginPage from './Pages/LoginPage';
+import SignUpPage from './Pages/SignUpPage';
 
 // admin screens
 import OrderScreen from './Admin/OrderScreen';
 
 export default function App() {
-  return (<OrderScreen />);
+  return (<SignUpPage />);
 }
