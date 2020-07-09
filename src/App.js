ef78e113cbb6770a40dc77d4d247d7181dbc5578
Fahad Ahmed <fhdplsh032@gmail.com>
2020-07-09 21:49:16 +0600

    Checkout page.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import ProductDetails from './Pages/ProductDetailsPage';
 import OrderPage from './Pages/OrderPage';
 import LoginPage from './Pages/LoginPage';
 import SignUpPage from './Pages/SignUpPage';
+import CheckoutPage from './Pages/CheckoutPage';
 
 // admin screens
 import OrderScreen from './Admin/OrderScreen';
 
 export default function App() {
-  return (<SignUpPage />);
+  return (<CheckoutPage />);
 }
